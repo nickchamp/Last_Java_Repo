49918372f8b5d677b12c1ecb0774955575eb3169
Nick Pruchkovskiy <nick.pruchkovskiy@gmail.com>
2018-02-20 18:06:34 +0200

    Changes with equality

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/ApplicationManager.java
@@ -22,11 +22,11 @@ public class ApplicationManager {
 
     public void init() {
         System.setProperty("webdriver.gecko.driver", "C:\\\\gecko\\geckodriver.exe");
-        if (browser == BrowserType.FIREFOX) {
+        if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
-        } else if (browser == BrowserType.CHROME) {
+        } else if (browser.equals(BrowserType.CHROME)) {
             wd = new ChromeDriver();
-        } else if (browser == BrowserType.IE) {
+        } else if (browser.equals(BrowserType.IE)) {
             wd = new InternetExplorerDriver();
         }
         wd.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
