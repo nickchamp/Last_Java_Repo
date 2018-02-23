e046a27005dd68b232c6d705219df01409bf4f87
Nick Pruchkovskiy <nick.pruchkovskiy@gmail.com>
2018-02-23 16:54:11 +0200

    Preconditions for Groups

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/NavigationHelper.java
@@ -2,7 +2,6 @@ package ru.stqa.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class NavigationHelper extends HelperBase {
 
@@ -12,7 +11,18 @@ public class NavigationHelper extends HelperBase {
     }
 
     public void gotoGroupCreationPage() {
+      if (isElementPresent(By.tagName("h1"))
+           && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+           && isElementPresent(By.name("new"))) {
+           return;
+      }
+      click(By.linkText("groups"));
+    }
 
-        click(By.linkText("groups"));
+    public void gotoHomePage() {
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        }
+        click(By.linkText("home"));
     }
 }
