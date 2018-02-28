fdbe1ef48feec66ecd89aedfbcf031eaa2ec4400
Nick Pruchkovskiy <nick.pruchkovskiy@gmail.com>
2018-02-28 13:34:04 +0200

    List Count Checker

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupDeletionTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.addressbook.model.GroupData;
 
@@ -7,6 +8,7 @@ public class GroupDeletionTests extends TestBase {
 
     @Test
     public void testGroupDeletion() {
+        int before = app.getGroupHelper().getGroupCount();
         app.getNavigationHelper().gotoGroupCreationPage();
         if (! app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
@@ -14,6 +16,8 @@ public class GroupDeletionTests extends TestBase {
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before - 1);
     }
 
 }
