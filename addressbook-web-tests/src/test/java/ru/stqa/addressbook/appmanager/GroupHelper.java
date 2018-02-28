05f14727713f8a44fbfb51bbaab43cf403e4cc18
Nick Pruchkovskiy <nick.pruchkovskiy@gmail.com>
2018-02-28 19:07:03 +0200

    ID Checking for GroupCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/GroupHelper.java
@@ -3,7 +3,6 @@ package ru.stqa.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.addressbook.model.GroupData;
 
 import java.util.ArrayList;
@@ -70,7 +69,7 @@ public class GroupHelper extends HelperBase {
         List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
         for (WebElement element : elements) {
             String name = element.getText();
-            String id = element.findElement(By.tagName("input")).getAttribute("value");
+            int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
             GroupData group = new GroupData(id, name, null, null);
             groups.add(group);
         }
